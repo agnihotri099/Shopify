202f6e96c53480c41981c9b7aa574255c9fa5cf0
Rishabh Agnihotri <rishabh.agnihotri@beamstacks.com>
2023-08-21 18:08:37 +0530

    main

--- shopify/src/containers/ProductComponent.js
+++ shopify/src/containers/ProductComponent.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
+
+  if (!products || products.length === 0) {
+    return <div>No products available.</div>;
+  }
+
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
       <div className="four wide column" key={id}>
         <Link to={`/product/${id}`}>
-          <div className="ui link cards">
-            <div className="card">
-              <div className="image">
-                <img src={image} alt={title} />
-              </div>
-              <div className="content">
-                <div className="header">{title}</div>
-                <div className="meta price">${price}</div>
-                <div className="meta">{category}</div>
-              </div>
+          <div className="ui link card">
+            <div className="image">
+              <img
+                src={image}
+                style={{
+                  resizeMode: "contain",
+                  height: 200,
+                  width: 290,
+                }}
+                alt={title}
+              />
+            </div>
+            <div className="content">
+              <h4 className="ui   header" >{title}</h4>
+              <div className="meta price">${price}</div>
+              <div className="meta">{category}</div>
             </div>
           </div>
         </Link>
@@ -26,7 +38,7 @@ const ProductComponent = () => {
     );
   });
 
-  return <>{renderList}</>;
+  return <div className="ui four column grid">{renderList}</div>;
 };
 
 export default ProductComponent;
